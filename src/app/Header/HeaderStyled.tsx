f3348884740a7700e034bcffcc4f9930c52e9a03
Diana Vidal <dvidaal@gmail.com>
2023-02-19 13:57:34 +0100

    Create header component and add styles

---
+++ src/app/Header/HeaderStyled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  .headerContainer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 30px;
+  }
+`;
+export default HeaderStyled;
